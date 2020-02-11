3790b5b691ed9dac43dfeb42e1997c1474ed1edd
Malvina Kalashnikova <malvinabluehair256@gmail.com>
2020-02-11 22:00:13 +0300

    Add new old files

---
+++ python_days/04 os import/MESSAGE.py
@@ -0,0 +1,13 @@
+def print_msg():
+    print("WE ARE FROM ANOTHER UNIVERSE. WE ARE SPACE UNICORNS!\n\n")
+    d = """
+                    /
+                  _/
+                |  |
+               /    \\
+              =========
+        
+         FRIENDSHIP IS MAGIC
+        
+        """
+    print(d)
---
+++ python_days/04 os import/cls.py
@@ -0,0 +1,3 @@
+from os import system as S
+S("cal && echo 'Bash пришёл с миром'")
+S("mpv --no-video music")
