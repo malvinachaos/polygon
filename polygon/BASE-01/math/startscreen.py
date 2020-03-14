78f327bddc8245d7e11839c281065d388c4ee608
Marina Kalashnikova <malvinabluehair256@gmail.com>
2020-03-14 17:58:57 +0300

    Edit some moments in economic

--- polygon/BASE-01/math/startscreen.py
+++ polygon/BASE-01/math/startscreen.py
@@ -1,4 +1,4 @@
 def load(name, comment, sec):
-    print("\t{}\n\tMADE BY SPACE UNICORN\n\n\t{}\n\n".format(name, comment), end='')
+    print("\n\t\t{}\n\t\tMADE BY SPACE UNICORN\n\n\t{}\n\n".format(name, comment), end='')
     import os
     os.system("sleep {}s;clear".format(sec))
