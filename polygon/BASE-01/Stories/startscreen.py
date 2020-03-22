5660430e458623f924bfcccef5314c76df8a5444
Marina Kalashnikova <malvinabluehair256@gmail.com>
2020-03-22 07:42:20 +0000

    Modify Dramatic and edit future.txt

---
+++ polygon/BASE-01/Stories/startscreen.py
@@ -0,0 +1,4 @@
+def load(name, comment, sec):
+    print("\n\t\t{}\n\t\tMADE BY SPACE UNICORN\n\n\t{}\n\n".format(name, comment), end='')
+    import os
+    os.system("sleep {}s;clear".format(sec))
