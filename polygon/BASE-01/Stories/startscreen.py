c053c0d95a84f3d075d20f3fc3ea1522ae5acf97
Marina Kalashnikova <malvinabluehair256@gmail.com>
2020-03-23 13:34:29 +0300

    Add Dramatic into psycho. In psycho add epic function. Edit startscreen.py and story.txt

--- polygon/BASE-01/Stories/startscreen.py
+++ polygon/BASE-01/Stories/startscreen.py
@@ -1,4 +1,4 @@
 def load(name, comment, sec):
-    print("\n\t\t{} All rights are left\n\t\tMADE BY SPACE UNICORN\n\n\t{}\n\n".format(name, comment), end='')
+    print("\n\t\t{} All rights are left\n\t\tMADE BY SPACE UNICORN\n\n\t\t{}\n\n".format(name, comment), end='')
     import os
     os.system("sleep {}s;clear".format(sec))
