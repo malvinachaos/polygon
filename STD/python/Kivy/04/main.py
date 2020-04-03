bc91a888f7238bf56157f192c9b296b1e9b5d1ed
Marina The Dragon <malvinabluehair256@gmail.com>
2020-04-03 23:17:17 +0300

    Continue Kivy learn

---
+++ STD/python/Kivy/04/main.py
@@ -0,0 +1,14 @@
+#! /bin/python3
+from kivy.app import App
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.uix.widget import Widget
+
+
+# ---------------------------[Блок запуска программы]--------------------------
+class DrawingApp(App):
+	def build(self):
+		return 0
+
+if __name__ == "__main__":
+		DrawingApp().run()
