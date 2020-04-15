f9fb39c1e9d6a64c3e19a1f9b37f4351d6f7b796
Marina The Dragon <malvinabluehair256@gmail.com>
2020-04-15 13:31:48 +0300

    Learn KIVYKIVY; cd 05; vim main.py!

--- STD/python/Kivy/05/main.py
+++ STD/python/Kivy/05/main.py
@@ -1,1 +1,12 @@
 #! /usr/bin/python3
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+class MyApp(App):
+	def build():
+		return 0
+
+
+if __name__ == "__main__":
+		MyApp().run()
