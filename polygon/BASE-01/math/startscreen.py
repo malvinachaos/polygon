d630c5814e1ac80b929d85e7116e806dd1cef1f8
Malvina Chaos <malvinabluehair256@gmail.com>
2020-06-23 12:13:08 +0300

    Полностью переделан BASE-01. Все программы переписаны и адаптированы в качестве функций в одну. Удалены лишние файлы. Модифицировала SUN, убрала из него модуль os

--- polygon/BASE-01/math/startscreen.py
+++
@@ -1,4 +0,0 @@
-def load(name, comment, sec):
-    print("\n\t\t{}\n\t\tMADE BY SPACE UNICORN\n\n\t{}\n\n".format(name, comment), end='')
-    import os
-    os.system("sleep {}s;clear".format(sec))
