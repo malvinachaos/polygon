f4a822ae2f5279598a6c8c0926e722c0b28403c7
Marina The Dragon <malvinabluehair256@gmail.com>
2020-10-29 11:38:09 +0300

    4th is done, need to check

--- STD/pascal/labs/4/main.pas
+++ STD/pascal/labs/4/main.pas
@@ -1,5 +1,73 @@
 PROGRAM by_Marina;
 
-BEGIN
 
+TYPE onedim = array of integer;
+
+VAR Af, Cf, out: text;
+    A, C: onedim;
+    n: integer;
+
+PROCEDURE arr_i(var f: text; var x: onedim; n: integer);
+Var i: integer;
+Begin
+    reset(f);
+    setlength(x, n-1);
+
+    for i:= 0 to n-1 do
+        read(f, x[i]);
+
+    close(f);
+End;
+
+PROCEDURE arr_o(var f: text; x: onedim; n: integer);
+Var i: integer;
+Begin
+    for i:= 0 to n-1 do
+        write(f, x[i], ' ');
+End;
+
+FUNCTION  find_num(x: onedim; y: onedim; n: integer):integer;
+Var i, num: integer; mini: integer; chibi: integer;
+Begin
+    mini:= x[1] - y[1];
+    for i:= 1 to n-2 do
+    begin
+        chibi:= x[i] - y[i];
+        if (chibi < (x[i-1] - y[i-1])) and (chibi < (x[i+1] - x[i+1])) and (chibi < mini) then
+        begin
+            mini:= chibi;
+            num:= i+1;
+        end;
+    end;
+    find_num:= num;
+End;
+
+BEGIN
+    if (argv[1] <> '') and (argv[2] <> '') then
+    begin
+        assign(Af, argv[1]);
+        assign(Cf, argv[2]);
+        assign(out, 'out.txt');
+        rewrite(out);
+
+        repeat
+            write('Введите количество элементов(больше 2, но меньше 30): ');
+            readln(n);
+        until((2 < n) and (n < 50));
+
+        arr_i(Af, A, n);
+        write(out, 'Значения массива A:', #13#10);
+        arr_o(out, A, n);
+
+        arr_i(Cf, C, n);
+        write(out, #13#10, #13#10, 'Значения массива C:');
+        arr_o(out, C, n);
+
+        write(out, #13#10, #13#10, 'Номер наименьшего из значений A^2 - C^2:', #13#10);
+        write(out, find_num(A, C, n));
+
+        close(out);
+    end
+    else writeln(#13#10, 'Использование:', #13#10,
+        './main file1.txt file2.txt [out_file.txt]');
 END.
